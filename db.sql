b70820fd3a99403397d4987aaac043d99eddcd13
mtamilarasi589 <67828752+mtamilarasi589@users.noreply.github.com>
2024-11-07 09:45:54 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,13 +1,20 @@
+static
+templates
+pip install flask
+pip install flask_mysqldb
+
+
 create database emp;
 use emp;
 CREATE TABLE empdetails
 (
-	id VARCHAR(10) primary key,
-   name VARCHAR(45) not null,
-  email VARCHAR(30) not null,
-  phone int(10) not null
+  id VARCHAR(10) primary key,
+  name VARCHAR(45) not null,
+  email VARCHAR(80) not null,
+  dept VARCHAR(50) not null
   );
-insert into empdetails values('e001','Raj','raj@gmail.com',9750037022);
-insert into empdetails values('e002','Sakthi','sakthi@gmail.com',9712345678);
-insert into empdetails values('e003','Shiva','shiva@gmail.com',9678945611);
+insert into empdetails values('e001','Raj','raj@gmail.com','HR');
+insert into empdetails values('e002','Sakthi','sakthi@gmail.com','Development');
+insert into empdetails values('e003','Shiva','shiva@gmail.com','Testing');
 select * from empdetails;
+
